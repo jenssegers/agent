2527cfd8ad6e73a126b3f906481f9bf273286e15
Vitaly Baev <dj@vitalybaev.ru>
2016-05-04 09:54:57 +0300

    Added Vivaldi browser detection

--- src/Agent.php
+++ src/Agent.php
@@ -37,6 +37,7 @@ class Agent extends Mobile_Detect {
      * @var array
      */
     protected static $additionalBrowsers = array(
+        'Vivaldi'           => 'Vivaldi',
         'Opera'             => 'Opera|OPR',
         'Edge'              => 'Edge',
         'Chrome'            => 'Chrome',
