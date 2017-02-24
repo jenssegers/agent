1687af8a84ee715acdc980e776a841e23cdf0f7c
Jens Segers <segers.jens@gmail.com>
2017-02-24 15:03:17 +0100

    Use bootstrap file for phpunit compatibility

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+if (! class_exists('PHPUnit_Framework_TestCase')) {
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
+
+if (! class_exists('PHPUnit\Framework\TestCase')) {
+    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
