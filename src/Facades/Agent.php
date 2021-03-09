c04001b9b3fe05dacf3b61e51980f7c25f20555f
Joel Butcher <joelbutcher@users.noreply.github.com>
2021-03-09 10:18:18 +0000

    Add method docs to facade

--- src/Facades/Agent.php
+++ src/Facades/Agent.php
@@ -4,6 +4,25 @@ namespace Jenssegers\Agent\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array getRules()
+ * @method static \Jaybizzle\CrawlerDetect\CrawlerDetect getCrawlerDetect()
+ * @method static array getBrowsers()
+ * @method static array getOperatingSystems()
+ * @method static array getPlatforms()
+ * @method static array getDesktopDevices()
+ * @method static array getProperties()
+ * @method static array languages()
+ * @method static string|bool browser()
+ * @method static string|bool platform()
+ * @method static string|bool device()
+ * @method static bool isDesktop()
+ * @method static bool isPhone()
+ * @method static string|bool robot()
+ * @method static bool isRobot()
+ * @method static string deviceType()
+ * @method static string|bool version($propertyName, $type = 'string')
+ */
 class Agent extends Facade
 {
     /**
